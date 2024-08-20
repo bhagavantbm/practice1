a720c732734714be62353099f843cd231f43596a
bhagavantbm <bhagavantmuragundi@gmail.com>
2024-08-20 08:16:59 +0530

    code

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,25 @@
+
+import { CardContainer } from "../cards/CardContainer"
 import { Button } from "../components/Botton"
+import { Mycarosal } from "../components/carosal/Mycarosal"
+import { MuiButton } from "../components/Mui/MuiButton"
+import { CustomStyle } from "../components/styles/CustomStyle"
+import "../components/styles/style.css"
+
 
 export const Home=()=>{
     return <>
-    <h1>I am Home page</h1>
+     
+    <h1 className="pqrs">Home </h1>
+    <Mycarosal/>
     <Button/>
+    <CardContainer/>
+    <CardContainer/>
+
+    <CardContainer/>
+    <CardContainer/>
+
+   
+  
 </>
 }
